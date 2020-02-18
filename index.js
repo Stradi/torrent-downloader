3b914af0bf511378780cb80a97f8d338d05d9bcc
Batin EVIRGEN <batinevirgen@gmail.com>
2020-02-18 14:05:42 +0300

    Added support for downloading videos in json file.

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const download = require("./lib/download")
+const parseJson = require("./lib/jsonParser").parseJSON;
+
 const argv = require("yargs")
 .usage("Usage: $0 <command> [options]")
 .command("download", "Download the specified torrent", (yargs) => {
-  yargs.positional("magnet", {
+  return yargs.option("magnet", {
+    alias: "m",
     type: "string",
     describe: "magnet uri to download",
-    demandOption: false
+    demandOption: true
   });
 }, (argv) => {
   download(argv.magnet);
+})
+.command("parse", "Parse the json file and download", (yargs) => {
+  return yargs.option("path", {
+    alias: "p",
+    type: "string",
+    describe: "path of the json file",
+    demandOption: true
+  });
+}, (argv) => {
+  parseJson(argv.path).then((data) => {
+    download(data.videos[0].magnet);
+  });
 })
 .argv;
